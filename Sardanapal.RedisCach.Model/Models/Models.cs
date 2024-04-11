e2edbb8a63d545d247c2cce02deb2d6fe870d80b
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-04-11 16:29:39 +0330

    Add: New package redis cach model has been added

---
+++ Sardanapal.RedisCach.Model/Models/Models.cs
@@ -0,0 +1,10 @@
+﻿
+using Sardanapal.DomainModel.Domain;
+
+namespace Sardanapal.RedisCach.Models;
+
+public interface ICachModel<TKey> : IBaseEntityModel<TKey>
+    where TKey : IEquatable<TKey>, IComparable<TKey>
+{
+
+}
