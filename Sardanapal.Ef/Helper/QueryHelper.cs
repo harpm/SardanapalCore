838afd05e113a957ab257f3066d2217cdf319988
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-04-11 12:08:51 +0330

    Update: Ef package optimized

--- Sardanapal.Ef/Helper/QueryHelper.cs
+++ Sardanapal.Ef/Helper/QueryHelper.cs
@@ -1,42 +1,36 @@
 ﻿using Sardanapal.DomainModel.Attributes;
 using Sardanapal.ViewModel.Models;
+using Sardanapal.Share.Extensions;
 using System.Reflection;
 
-namespace Sardanapal.Ef.Helper
+namespace Sardanapal.Ef.Helper;
+
+public static class QueryHelper
 {
-    public static class QueryHelper
+    public static IQueryable<TEntity> Search<TEntity>(this IQueryable<TEntity> query, GridSearchModelVM searchModel = null)
     {
-        public static IQueryable<TEntity> Search<TEntity>(this IQueryable<TEntity> query, GridSearchModelVM searchModel = null)
-        {
-            if (searchModel == null)
-                return query;
-
-            if (string.IsNullOrWhiteSpace(searchModel.SortId))
-            {
-                var opt = (EntityOptions)typeof(TEntity).GetCustomAttribute(typeof(EntityOptions));
-                if (opt != null)
-                {
-                    searchModel.SortId = opt.OrderBy;
-                }
-            }
-
-            if (!string.IsNullOrWhiteSpace(searchModel.SortId))
-            {
-                query = query.OrderBy(x => x.GetType().GetProperty(searchModel.SortId).GetValue(x));
-            }
-
-            if (searchModel.PageSize > 0)
-            {
-                query = query.Page(searchModel.PageIndex, searchModel.PageSize);
-            }
-
+        if (searchModel == null)
             return query;
+
+        if (string.IsNullOrWhiteSpace(searchModel.SortId))
+        {
+            var opt = (EntityOptions)typeof(TEntity).GetCustomAttribute(typeof(EntityOptions));
+            if (opt != null)
+            {
+                searchModel.SortId = opt.OrderBy;
+            }
         }
 
-        public static IQueryable<T> Page<T>(this IQueryable<T> query, int pageIndex, int pageSize)
+        if (!string.IsNullOrWhiteSpace(searchModel.SortId))
         {
-            return query.Skip(pageSize * pageIndex)
-                    .Take(pageSize);
+            query = query.OrderBy(x => x.GetType().GetProperty(searchModel.SortId).GetValue(x));
         }
+
+        if (searchModel.PageSize > 0)
+        {
+            query = query.Page(searchModel.PageIndex, searchModel.PageSize);
+        }
+
+        return query;
     }
 }
