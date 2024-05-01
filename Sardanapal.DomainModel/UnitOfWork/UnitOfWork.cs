6306ac2366106b135af766df68158e582e27bf2e
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-05-01 12:04:02 +0330

    Add: Implemented constructor base in UOW

--- Sardanapal.DomainModel/UnitOfWork/UnitOfWork.cs
+++ Sardanapal.DomainModel/UnitOfWork/UnitOfWork.cs
@@ -3,68 +3,73 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Sardanapal.DomainModel.Domain;
 using System.Reflection;
 
-namespace Sardanapal.DomainModel.UnitOfWork
+namespace Sardanapal.DomainModel.UnitOfWork;
+
+public interface ISardanapalUnitOfWork
 {
-    public interface ISardanapalUnitOfWork
+    Type[] GetDomainModels();
+    void ApplyFluentConfigs<T>(EntityTypeBuilder entity) where T : class, IDomainModel;
+}
+
+public abstract class SardanapalUnitOfWork : DbContext, ISardanapalUnitOfWork
+{
+    public SardanapalUnitOfWork(DbContextOptions<SardanapalUnitOfWork> opt)
+        : base(opt)
     {
-        Type[] GetDomainModels();
-        void ApplyFluentConfigs<T>(EntityTypeBuilder entity) where T : class, IDomainModel;
+
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        foreach (Type t in GetDomainModels())
+        {
+            var entity = builder.GetType().GetMethods()
+                .Where(x => x.IsGenericMethod && x.Name == nameof(ModelBuilder.Entity))
+                .First()
+                .MakeGenericMethod(t)
+                .Invoke(builder, null);
+            this.GetType().GetMethod(nameof(ApplyFluentConfigs))?.MakeGenericMethod(t).Invoke(this, new[] { entity });
+        }
+
+        base.OnModelCreating(builder);
     }
 
-    public abstract class SardanapalUnitOfWork : DbContext, ISardanapalUnitOfWork
+    public virtual Type[] GetDomainModels()
     {
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            foreach (Type t in GetDomainModels())
-            {
-                var entity = builder.GetType().GetMethods()
-                    .Where(x => x.IsGenericMethod && x.Name == nameof(ModelBuilder.Entity))
-                    .First()
-                    .MakeGenericMethod(t)
-                    .Invoke(builder, null);
-                this.GetType().GetMethod(nameof(ApplyFluentConfigs))?.MakeGenericMethod(t).Invoke(this, new[] { entity });
-            }
+        return Assembly.GetExecutingAssembly().GetTypes()
+            .Where(x => x.IsAssignableTo(typeof(IDomainModel)))
+            .ToArray();
+    }
 
-            base.OnModelCreating(builder);
+    public virtual void ApplyFluentConfigs<T>(EntityTypeBuilder entity)
+        where T : class, IDomainModel
+    {
+        var FluentType = typeof(T).Assembly.GetTypes()
+            .Where(x => x.IsAssignableTo(typeof(FluentModelConfig<>).MakeGenericType(typeof(T))))
+            .FirstOrDefault();
+
+        if (FluentType != null)
+        {
+            var fluentConfig = FluentType.GetConstructor(null).Invoke(null) as FluentModelConfig<T>;
+
+            var OnModelBuild = FluentType.GetMethod("OnModelBuild");
+            OnModelBuild.Invoke(null, new object[] { entity });
         }
+    }
 
-        public virtual Type[] GetDomainModels()
+    protected virtual void SetBaseValues()
+    {
+        var EntityModels = ChangeTracker
+            .Entries()
+            .Where(e => typeof(ILogicalEntityModel).IsAssignableFrom(e.Entity.GetType()) && (e.State == EntityState.Deleted))
+            .ToArray();
+
+        foreach (var model in EntityModels)
         {
-            return Assembly.GetExecutingAssembly().GetTypes()
-                .Where(x => x.IsAssignableTo(typeof(IDomainModel)))
-                .ToArray();
-        }
+            ILogicalEntityModel entity = (ILogicalEntityModel)model.Entity;
 
-        public virtual void ApplyFluentConfigs<T>(EntityTypeBuilder entity)
-            where T : class, IDomainModel
-        {
-            var FluentType = typeof(T).Assembly.GetTypes()
-                .Where(x => x.IsAssignableTo(typeof(FluentModelConfig<>).MakeGenericType(typeof(T))))
-                .FirstOrDefault();
-
-            if (FluentType != null)
-            {
-                var fluentConfig = FluentType.GetConstructor(null).Invoke(null) as FluentModelConfig<T>;
-
-                var OnModelBuild = FluentType.GetMethod("OnModelBuild");
-                OnModelBuild.Invoke(null, new object[] { entity });
-            }
-        }
-
-        protected virtual void SetBaseValues()
-        {
-            var EntityModels = ChangeTracker
-                .Entries()
-                .Where(e => typeof(ILogicalEntityModel).IsAssignableFrom(e.Entity.GetType()) && (e.State == EntityState.Deleted))
-                .ToArray();
-
-            foreach (var model in EntityModels)
-            {
-                ILogicalEntityModel entity = (ILogicalEntityModel)model.Entity;
-
-                entity.IsDeleted = true;
-                model.State = EntityState.Modified;
-            }
+            entity.IsDeleted = true;
+            model.State = EntityState.Modified;
         }
     }
 }
