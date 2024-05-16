8275f3e47bcaed2d1857e47a9a882b3d0ad19c29
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-05-17 00:46:38 +0330

    Update: RedisCach package

---
+++ Sardanapal.RedisCach/Models/Interface.cs
@@ -0,0 +1,10 @@
+﻿
+using Sardanapal.Domain.Model;
+
+namespace Sardanapal.RedisCach.Models;
+
+public interface ICachModel<TKey> : IBaseEntityModel<TKey>
+    where TKey : IEquatable<TKey>, IComparable<TKey>
+{
+
+}
