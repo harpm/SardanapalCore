62b78429e965f44d0ed7c1a322281b6755c8def1
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-06-14 01:59:12 +0330

    Fix: some minor issues in the services constraints

--- Sardanapal.RedisCach.Service/Services/IService.cs
+++ Sardanapal.RedisCach.Service/Services/IService.cs
@@ -1,6 +1,5 @@
 ﻿using Sardanapal.Domain.Model;
 using Sardanapal.Interface.IService;
-using Sardanapal.RedisCach.Models;
 using Sardanapal.ViewModel.Response;
 
 namespace Sardanapal.RedisCache.Services;
@@ -10,9 +9,9 @@ public interface ICacheService<TModel, TKey, TSearchVM, TVM, TNewVM, TEditableVM
     where TModel : IBaseEntityModel<TKey>, new()
     where TKey : IEquatable<TKey>, IComparable<TKey>
     where TSearchVM : class, new()
-    where TVM : class, ICachModel<TKey>, new()
-    where TNewVM : class, ICachModel<TKey>, new()
-    where TEditableVM : class, ICachModel<TKey>, new()
+    where TVM : class, new()
+    where TNewVM : class, new()
+    where TEditableVM : class, new()
 {
     Task<IResponse<TKey>> Add(TModel model);
 }
