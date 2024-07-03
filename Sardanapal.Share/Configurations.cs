00a087558b52c1476b14bc257bd24e2be7f7c729
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-07-03 13:52:01 +0330

    Add: AutoMapper injection method

---
+++ Sardanapal.Share/Configurations.cs
@@ -0,0 +1,29 @@
+﻿
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Sardanapal.Share;
+
+public static class SharedConfigurations
+{
+    public static void AddAutoMapper(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddScoped<IConfigurationProvider>(sp =>
+        {
+            return new MapperConfiguration(config =>
+            {
+                config.AddProfiles(assemblies
+                    .SelectMany(x => x.GetTypes()
+                        .Where(t => t.IsSubclassOf(typeof(Profile)) && !t.IsAbstract)
+                        .Select(t => t.GetConstructors().First().Invoke(null) as Profile)
+                        .ToArray()));
+            });
+        });
+
+        services.AddScoped<IMapper>(sp =>
+        {
+            return new Mapper(sp.GetRequiredService<IConfigurationProvider>(), sp.GetService);
+        });
+    }
+}
