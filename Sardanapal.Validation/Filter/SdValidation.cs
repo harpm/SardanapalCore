5200a5c92fb79cbc450f6a4cb0bc4075b073b3fb
Arsalan Hadidi <arsalanh@systemgroup.net>
2024-08-14 15:45:38 +0330

    impl: added http filter for validation

---
+++ Sardanapal.Validation/Filter/SdValidation.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Sardanapal.Interface.IService;
+using Sardanapal.ViewModel.Response;
+using System.Net;
+
+namespace Sardanapal.Validation.Filter;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+public class SdValidation : ActionFilterAttribute
+{
+    public SdValidation()
+    {
+
+    }
+
+    public override async void OnActionExecuting(ActionExecutingContext action)
+    {
+        base.OnActionExecuting(action);
+
+        try
+        {
+            IValidationService validationService = action.HttpContext.RequestServices.GetService(typeof(IValidationService)) as IValidationService;
+
+            if (validationService == null)
+                throw new NullReferenceException(string.Format("Requiered Service {0} cannot be resolved."
+                    , nameof(IValidationService)));
+
+            if (!validationService.IsProceeded)
+            {
+                var parameters = action.ActionDescriptor.Parameters;
+
+                if (parameters != null && parameters.Any())
+                {
+                    for (var i = 0; i < parameters.Count; i++)
+                    {
+                        if (parameters[i].ParameterType == typeof(CancellationToken))
+                        {
+                            continue;
+                        }
+
+                        validationService.ValidateParams(parameters.Select(p => p.ParameterType).ToArray()
+                            , action.ActionArguments.Select(a => a.Value).ToArray());
+
+                    }
+                }
+            }
+            if (!validationService.IsValid)
+            {
+                IResponse<object> response = new Response<object>()
+                {
+                    StatusCode = StatusCode.Exception,
+                    DeveloperMessages = validationService.Messages.ToArray()
+                };
+                action.Result = new BadRequestObjectResult(response);
+            }
+        }
+        catch (Exception ex)
+        {
+            action.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            action.Result = new BadRequestResult();
+        }
+    }
+}
