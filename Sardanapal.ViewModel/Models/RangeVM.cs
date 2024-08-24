dd5e2dd5ae9f2a80d0efd3d440e869c3d26cef99
ArsalanH <arsalanh@systemgroup.net>
2024-08-24 15:39:47 +0330

    impl: RangeVM model view

---
+++ Sardanapal.ViewModel/Models/RangeVM.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Sardanapal.ViewModel.Model;
+
+public class RangeVM<T>
+    where T : IComparable<T>, IEquatable<T>
+{
+    public T Start { get; set; }
+    public T End { get; set; }
+
+    public bool Contains(T item)
+    {
+        return item.CompareTo(Start) >= 0 && item.CompareTo(End) <= 0;
+    }
+}
