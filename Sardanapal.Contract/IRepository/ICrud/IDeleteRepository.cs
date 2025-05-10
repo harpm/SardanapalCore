c18ce2517606d1ea272995d6c569b8166cc83b06
ArsalanH <arsalanh@systemgroup.net>
2025-05-10 15:19:04 +0330

    add: required fundamental contracts for repository pattern [Contract]

---
+++ Sardanapal.Contract/IRepository/ICrud/IDeleteRepository.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Sardanapal.Contract.IRepository.ICrud;
+
+public interface IDeleteRepository<TKey>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+{
+    bool Delete(TKey key);
+    Task<bool> DeleteAsync(TKey key);
+}
---
+++ Sardanapal.Contract/IRepository/ICrud/IUpdateRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using Sardanapal.Contract.IModel;
+
+namespace Sardanapal.Contract.IRepository.ICrud;
+
+public interface IUpdateRepository<TKey, TModel>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+    where TModel : class, IDomainModel, new()
+{
+    bool Update(TKey key, TModel model);
+    Task<bool> UpdateAsync(TKey key, TModel model);
+}
