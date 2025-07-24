49229c63c916f67063d51a1cc353a6969e16d65d
harpm <arsalan.hadidi1379@gmail.com>
2025-07-24 17:18:55 +0330

    fix: edit issue in Crud Base Service

--- Sardanapal.Service/CrudService.cs
+++ Sardanapal.Service/CrudService.cs
@@ -78,8 +78,9 @@ public abstract class CrudServiceBase<TRepository, TKey, TEntity, TSearchVM, TVM
 
         await result.FillAsync(async () =>
         {
-            var editedModel = _mapper.Map<TEditableVM, TEntity>(Model);
-            var data = await _repository.UpdateAsync(Id, editedModel, ct);
+            var entity = await _repository.FetchByIdAsync(Id, ct);
+            _mapper.Map<TEditableVM, TEntity>(Model, entity);
+            var data = await _repository.UpdateAsync(Id, entity, ct);
             
             result.Set(data ? StatusCode.Succeeded : StatusCode.Failed, data);
         });
