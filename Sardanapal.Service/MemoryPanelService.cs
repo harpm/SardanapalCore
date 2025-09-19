c893f1feb8aca592cf0a451c64671c1f38fcf8bb
harpm <arsalan.hadidi1379@gmail.com>
2025-09-19 18:03:06 +0330

    add: memory panel service

---
+++ Sardanapal.Service/MemoryPanelService.cs
@@ -0,0 +1,60 @@
+
+using AutoMapper;
+using Sardanapal.Contract.IModel;
+using Sardanapal.Contract.IRepository;
+using Sardanapal.Contract.IService;
+using Sardanapal.Service.Utilities;
+using Sardanapal.ViewModel.Models;
+using Sardanapal.ViewModel.Response;
+
+namespace Sardanapal.Service;
+
+public abstract class MemoryPanelServiceBase<TRepository, TKey, TEntity, TSearchVM, TVM, TNewVM, TEditableVM>
+    : CrudServiceBase<TRepository, TKey, TEntity, TSearchVM, TVM, TNewVM, TEditableVM>
+    , IPanelService<TKey, TSearchVM, TVM, TNewVM, TEditableVM>
+    where TRepository : ICrudRepository<TKey, TEntity>, IMemoryRepository<TKey, TEntity>
+    where TKey : IComparable<TKey>, IEquatable<TKey>
+    where TEntity : class, IBaseEntityModel<TKey>, new()
+    where TSearchVM : class, new()
+    where TVM : class, new()
+    where TNewVM : class, new()
+    where TEditableVM : class, new()
+{
+    protected MemoryPanelServiceBase(TRepository repository, IMapper mapper)
+        : base(repository, mapper)
+    {
+        
+    }
+
+    public virtual async Task<IResponse<GridVM<TKey, SelectOptionVM<TKey, object>>>>
+            GetDictionary(GridSearchModelVM<TKey, TSearchVM> SearchModel = null, CancellationToken ct = default)
+    {
+        IResponse<GridVM<TKey, SelectOptionVM<TKey, object>>> result
+            = new Response<GridVM<TKey, SelectOptionVM<TKey, object>>>(ServiceName, OperationType.Fetch);
+
+        await result.FillAsync(async () =>
+        {
+            if (SearchModel is null)
+                SearchModel = new();
+            if (SearchModel.Fields is null)
+                SearchModel.Fields = new();
+
+            var data = new GridVM<TKey, SelectOptionVM<TKey, object>>(SearchModel);
+
+            var entities = await _repository.FetchAllAsync(ct);
+            entities = Search(entities, SearchModel.Fields);
+
+            data.SearchModel.TotalCount = entities.Count();
+
+            var list = EnumerableHelper.Search(entities, SearchModel)
+                .Select(e => _mapper.Map<SelectOptionVM<TKey, object>>(e))
+                .ToList();
+
+            data.List = list;
+
+            result.Set(StatusCode.Succeeded, data);
+        });
+
+        return result;
+    }
+}
