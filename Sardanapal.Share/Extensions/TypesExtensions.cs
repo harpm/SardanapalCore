bdf39a8fcfa7aea9c1a1f4ebe19b45f3a6f2644f
harpm <arsalan.hadidi1379@gmail.com>
2025-11-19 20:11:54 +0330

    resolve: un-filled props of the EntityModel (CreateBy, ModifiedBy and etc)

--- Sardanapal.Share/Extensions/TypesExtensions.cs
+++ Sardanapal.Share/Extensions/TypesExtensions.cs
@@ -1,8 +1,27 @@
-﻿
+
 namespace Sardanapal.Share.Extensions;
 
 public static class TypesExtensions
 {
+    public static bool ImplementsRawGeneric(Type type, Type generic)
+    {
+        // check interfaces
+        if (type.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == generic))
+            return true;
+
+        // check base classes too
+        while (type != null && type != typeof(object))
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == generic)
+                return true;
+
+            type = type.BaseType;
+        }
+
+        return false;
+    }
+
     public static bool IsSubClassOfRawGeneric(this Type generic, Type toCheck)
     {
         while (toCheck != null && toCheck != typeof(object))
